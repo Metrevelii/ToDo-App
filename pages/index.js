e349acc6086638da62e51266b434e409d0e22dcd
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-09 00:59:28 +0400

    added nextjs and tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className="flex justify-center flex-col items-center">
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
